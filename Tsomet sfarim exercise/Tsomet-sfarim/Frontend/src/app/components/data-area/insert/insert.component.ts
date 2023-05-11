8835143a56973ee7d98dc0b107f1421c367ff4d2
maxiboom1 <galutziogen@gmail.com>
2023-05-11 16:30:24 +0300

    add-book - in dev..

--- Tsomet sfarim exercise/Tsomet-sfarim/Frontend/src/app/components/data-area/insert/insert.component.ts
+++ Tsomet sfarim exercise/Tsomet-sfarim/Frontend/src/app/components/data-area/insert/insert.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import AuthorModel from 'src/app/models/AuthorModel';
+import BookModel from 'src/app/models/BookModel';
 import { DataService } from 'src/app/services/data.service';
 import { NotifyService } from 'src/app/services/notify.service';
 
@@ -7,10 +9,25 @@ import { NotifyService } from 'src/app/services/notify.service';
     templateUrl: './insert.component.html',
     styleUrls: ['./insert.component.css']
 })
-export class InsertComponent {
+export class InsertComponent implements OnInit {
 
     public constructor(private dataService: DataService, private notifyService: NotifyService) { }
+    
+    public authors: AuthorModel[];
+    public book: BookModel;
 
+    public async ngOnInit(): Promise<void> {
+        
+        try{
+            console.log('xxx')
+            this.authors = await this.dataService.getAllAuthors();
+        }catch(err:any){
+            this.notifyService.error(err);
+        }
+        
+    }
 
-
+    public send(){
+        console.log(this.book)
+    }
 }
