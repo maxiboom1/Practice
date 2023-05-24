2b1c0c5795085e86bee9af7b523ce80a836a4339
Alex Samih-zade <galutziogen@gmail.com>
2023-05-24 18:26:26 +0300

    PHP Exam

---
+++ Projects/PHP/PHP exam/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Trirong">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>PHP exam 24-05-2023</title>
+</head>
+
+
+<body>
+    <div class="content">
+    <h1>Google Search Proxy</h1>
+    <hr>
+    <span>Search for everything:</span>
+
+    <form action="search.php" method="get">
+                
+        <input type="text" name="searchQuery" required>
+        
+        <button>Search</button>
+
+    </form>
+
+    </div>
+
+
+    <footer>
+        <h5>Created by Alex.</h5>
+    </footer>
+</body>
+</html>
---
+++ Projects/PHP/PHP exam/search.php
@@ -0,0 +1,5 @@
+<?php
+
+$searchString = $_GET["searchQuery"];
+
+header("location: https://www.google.co.il/search?q=$searchString");
